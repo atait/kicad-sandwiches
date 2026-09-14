6f6d7cef7456c1cc504f36cb9f2b8caf6166e6de
Alex Tait <atait@ieee.org>
2026-09-14 15:28:57 -0400

    Cleared out old stuff

--- quick_reload/__init__.py
+++
@@ -1,3 +0,0 @@
-from .action_quickreload import QuickReload
-QuickReload().register() # Instantiate and register to Pcbnew
-
--- tests/__init__.py
+++ tests/__init__.py
@@ -7,5 +7,6 @@ import pytest
 import lytest
 # lytest.kdb_xor.run_xor = lytest.kdb_xor.run_xor_pcbnew
 lytest.utest_buds.default_file_ext = '.kicad_pcb'
-lytest.utest_buds.test_root = os.path.dirname(__file__)
-lytest.utest_buds.get_src_dir()
+from kigadgets import SWIG_version
+lytest.utest_buds.test_root = os.path.join(os.path.dirname(__file__), f'v{SWIG_version}')
+# lytest.utest_buds.get_src_dir()
